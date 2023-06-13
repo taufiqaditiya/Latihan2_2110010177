0f9f318aec70492df847af81638fe8fe05e99b32
taufiqaditiya <aditiya.rahman78@gmail.com>
2023-06-13 12:27:15 +0800

    Membuat class Matematika dan MatematikaBeraksi2

--- src/Universitas/Mahasiswa.java
+++ src/Universitas/Mahasiswa.java
@@ -1,10 +1,42 @@
-package Universitas;
+package universitas;
+
 
 public class Mahasiswa {
-    private String npm, nama, prodi;
-  
+  private String nama;
+  private String npm;
+  private String prodi;
 
-    public String getDetai(){
+    public Mahasiswa(String npm, String nama, String prodi) {
+        this.nama = nama;
+        this.npm = npm;
+        this.prodi = prodi;
+    }
+  
+    public String setDetail(){
         return npm + nama + prodi;
-}
+    }
+
+    public String getProdi() {
+        return prodi;
+    }
+
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
+    }
+    
+    public String getNama() {
+        return nama;
+    }
+    
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+    public String getNpm() {
+        return npm;
+    }
+    
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
 }
--- src/Universitas/MahasiswaBeraksi.java
+++ src/Universitas/MahasiswaBeraksi.java
@@ -1,7 +1,17 @@
-package Universitas;
+package universitas;
+
 
 public class MahasiswaBeraksi {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+            
+    Mahasiswa mhs = new Mahasiswa("2110010177", " M. Aditiya Taufiqurrahman", " Teknik Informatika");
+    
+    
+        System.out.println("Nama: "+mhs.getNama());
+        System.out.println("NPM: "+mhs.getNpm());
+        System.out.println("Prodi: "+mhs.getProdi());
         
-    }
+        System.out.println(mhs.setDetail());
+
+}
 }
